fac9bad0bfc357a4a44b7ad2db415cf9bf208f87
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2022-04-30 21:26:56 +0200

    fixed some bugs

--- drop_terminal.sh
+++ drop_terminal.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
-SCRATCH="$HOME/.drop_terminal"
+#::::::::: VARIABLES ::::::::::::::
+# Good for accessibility to have a the
+# possibility to view and edit the two
+# variables below
+term="kitty"
+
+#::::::::: FUNCTIONS ::::::::::::::
 
 function help {
     echo -e "USAGE\n=====\n- $0"
@@ -10,52 +16,67 @@ function help {
     exit
 }
 
-# check if the dropdown terminal is already open
-function is_running {
-    WID=`wmctrl -lx | grep "drop_terminal.drop_terminal" | grep -v "grep" | awk '{print $1}'`
-    [ -z "$WID" ] && echo 1 || echo 0
+# Check AND if its running and the status
+   # If the status function fails, it echo 2, it means than
+   # the WID is not found, so than the terminal is not running
+   # so as we want to show it, it spawns it...
+
+function status {
+	[ -z "$WID" ] && spawn && return
+
+    winfos=`xwininfo -id $WID -stats 2>/dev/null`
+    [ -z "$winfos" ] && echo 2 && return
+
+    state=`grep IsUnMapped <<< $winfos`
+    [ -n "$state" ] && echo 0 || echo 1
 }
 
 # spawn a new one if needeed
 function spawn {
-    kitty --class drop_terminal &
+    $term --class drop_terminal &
+    pid=$!
     sleep 0.5
-    WID=`wmctrl -lx | grep "drop_terminal.drop_terminal" | grep -v "grep" | awk '{print $1}'`
+    # More secure to grep the pid
+    WID=`wmctrl -lxp | grep $pid | grep -v "grep" | awk '{print $1}'`
     echo "$WID" > $SCRATCH
 }
 
 # hide the dropdown terminal
 function hide {
-    WID=`wmctrl -lx | grep "drop_terminal.drop_terminal" | grep -v "grep" | awk '{print $1}'`   
-    xdo hide $WID
-    echo "$WID" > $SCRATCH
+	 WID=`head -n 1 $SCRATCH 2>/dev/null`
+	 [ -z "$WID" ] && exit
+	 [ `status` -eq 1 ] && xdo hide $WID
     exit
 }
 
 # show the dropdown terminal
 function show {
-    WID=`head -n 1 $SCRATCH 2>/dev/null`
-    if [ `is_running` -eq 0 ]; then
-        exit
-    fi
-    if [ -z "$WID" ]; then
-        spawn &
-        exit
-    fi
-    xdo show $WID
+	 WID=`head -n 1 $SCRATCH 2>/dev/null`
+	 if [ `status` -eq 0 ]; then
+	 	  xdo show $WID
+	 	  xdo activate $WID
+	 elif [ `status` -eq 2 ]; then
+	 	  spawn
+	 fi
     exit
 }
 
 function toggle {
-    if [ `is_running` -eq 0 ]; then
+	 WID=`head -n 1 $SCRATCH 2>/dev/null`
+	 if [ `status` -eq 2 ]; then
+	 	  spawn
+	 elif [ `status` -eq 1 ]; then
         hide
     else
         show
     fi
 }
 
+#:::::::::: SCRIPT :::::::::::::::::
+
+SCRATCH="$HOME/.drop_terminal"
+
 [ $# -eq 0 ] && help
 [ "$1" = "hide" ] && hide
 [ "$1" = "show" ] && show
 [ "$1" = "toggle" ] && toggle
-
